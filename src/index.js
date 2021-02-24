c10fd8c9a329d799985cc42733ffda648651d762
ValerieKaziuchyts <kaziuchytsvalerie@gmail.com>
2021-02-25 00:24:52 +0300

    feat: implement brackets check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let pairBrackets  = [];
+
+    bracketsConfig.forEach(item => {
+        pairBrackets.push(item.join(''));
+    });
+
+    for (let i = 0; i < pairBrackets.length; i++) {
+        if (str.includes(pairBrackets[i])) {
+            str = str.replace(pairBrackets[i], "");
+            i = -1;
+        }
+    }
+    
+    return str.length === 0 ? true : false;
+}  
